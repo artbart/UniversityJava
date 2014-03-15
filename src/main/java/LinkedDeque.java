32e5458fd576e9a09dff41139157b330519e0168
artbart <ar.bartashev@gmail.com>
2014-03-15 13:36:55 +0400

    refactoring

--- src/main/java/LinkedDeque.java
+++ src/main/java/LinkedDeque.java
@@ -4,91 +4,95 @@ import java.util.*;
  * Created by artbart on 3/7/14.
  */
 public class LinkedDeque<E> implements Collection<E> {
-    private class Node{
-        E val;
-        Node next;
-        Node prev;
+    private class Node {
+        public E val;
+        public Node next;
+        public Node prev;
 
         private Node() {
         }
+
         private Node(E val) {
             this.val = val;
         }
     }
 
-    private class LinkedDequeIterator implements Iterator<E>{
+    private class LinkedDequeIterator implements Iterator<E> {
         private Node current;
         private boolean isRemoved;
 
         private LinkedDequeIterator() {
             this.current = head;
-            isRemoved=false;
+            isRemoved = false;
         }
 
         @Override
         public boolean hasNext() {
-            return current.next!=tail;
+            return current.next != tail;
         }
 
         @Override
         public E next() {
-            if (current.next==tail) throw new NoSuchElementException();
-            current=current.next;
-            isRemoved=false;
+            if (current.next == tail) throw new NoSuchElementException();
+            current = current.next;
+            isRemoved = false;
             return current.val;
         }
 
         @Override
         public void remove() {
-            if (isRemoved || current==head ) throw new IllegalStateException();
-            isRemoved=true;
+            if (isRemoved || current == head) throw new IllegalStateException();
+            isRemoved = true;
             removeNode(current);
         }
     }
 
     private int size;
-    private Node head;
-    private Node tail;
+    private final Node head;
+    private final Node tail;
 
 
     public LinkedDeque() {
-        size=0;
-        head=new Node();
-        tail=new Node();
-        head.next=tail;
-        tail.prev=head;
+        size = 0;
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
     }
 
     // deque
-    public void addFirst(E elem){
-        if (elem==null) throw  new NullPointerException();
-        Node node=new Node(elem);
+    public void addFirst(E elem) {
+        if (elem == null) throw new NullPointerException();
+        Node node = new Node(elem);
         insertAfter(head, node);
         size++;
     }
-    public void addLast(E elem){
+
+    public void addLast(E elem) {
         add(elem);
     }
 
-    public E getFirst(){
-        if (size==0) throw new NoSuchElementException();
+    public E getFirst() {
+        if (size == 0) throw new NoSuchElementException();
         return head.next.val;
     }
-    public E getLast(){
-        if (size==0) throw new NoSuchElementException();
+
+    public E getLast() {
+        if (size == 0) throw new NoSuchElementException();
         return tail.prev.val;
     }
 
 
-    public E removeFirst(){
-        if (size==0) throw new NoSuchElementException();
-        Node node=head.next;
+    public E removeFirst() {
+        if (size == 0) throw new NoSuchElementException();
+        Node node = head.next;
         removeNode(node);
         return node.val;
     }
-    public E removeLast(){
-        if (size==0) throw new NoSuchElementException();
-        Node node=tail.prev;
+
+    public E removeLast() {
+        if (size == 0) throw new NoSuchElementException();
+        Node node = tail.prev;
         removeNode(node);
         return node.val;
     }
@@ -103,12 +107,12 @@ public class LinkedDeque<E> implements Collection<E> {
 
     @Override
     public boolean isEmpty() {
-        return size==0;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        return findNode(o)!=null;
+        return findNode(o) != null;
     }
 
     @Override
@@ -118,42 +122,48 @@ public class LinkedDeque<E> implements Collection<E> {
 
     @Override
     public Object[] toArray() {
-        Object[] arr=new Object[size];
-        Iterator<E> it=iterator();
-        int ind=0;
-        while (it.hasNext()) arr[ind++]=it.next();
+        Object[] arr = new Object[size];
+        Iterator<E> it = iterator();
+        int ind = 0;
+        while (it.hasNext()) arr[ind++] = it.next();
         return arr;
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
-        T[] arr=(T[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
-        Iterator<E> it=iterator();
-        int ind=0;
-        while (it.hasNext()) arr[ind++]= (T) it.next();
+        if (a==null) throw new NullPointerException();
+        T[] arr;
+        if (a.length >= size()) {
+            arr = a;
+        } else {
+            arr = (T[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
+        }
+        Iterator<E> it = iterator();
+        int ind = 0;
+        while (it.hasNext()) arr[ind++] = (T) it.next();
         return arr;
     }
 
     @Override
     public boolean add(E elem) {
-        if (elem==null) throw  new NullPointerException();
-        Node node=new Node(elem);
-        insertBefore(tail,node);
+        if (elem == null) throw new NullPointerException();
+        Node node = new Node(elem);
+        insertBefore(tail, node);
         size++;
         return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        Node node=findNode(o);
-        if (node==null) return false;
+        Node node = findNode(o);
+        if (node == null) return false;
         removeNode(node);
         return true;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        for (Object o : c){
+        for (Object o : c) {
             if (!contains(o)) return false;
         }
         return true;
@@ -167,12 +177,12 @@ public class LinkedDeque<E> implements Collection<E> {
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        boolean modified=false;
-        for (Object o : c){
-            Node node=findNode(o);
-            if (node==null) continue;
+        boolean modified = false;
+        for (Object o : c) {
+            Node node = findNode(o);
+            if (node == null) continue;
             removeNode(node);
-            modified=true;
+            modified = true;
         }
         return modified;
     }
@@ -192,47 +202,47 @@ public class LinkedDeque<E> implements Collection<E> {
 
     @Override
     public void clear() {
-        head.next=tail;
-        tail.prev=head;
-        size=0;
+        head.next = tail;
+        tail.prev = head;
+        size = 0;
     }
 
-    private void insertBefore(Node oldNode, Node newNode){
-        newNode.prev=oldNode.prev;
-        newNode.next=oldNode;
+    private void insertBefore(Node oldNode, Node newNode) {
+        newNode.prev = oldNode.prev;
+        newNode.next = oldNode;
 
-        oldNode.prev.next=newNode;
-        oldNode.prev=newNode;
+        oldNode.prev.next = newNode;
+        oldNode.prev = newNode;
     }
 
-    private void insertAfter(Node oldNode, Node newNode){
-        newNode.next=oldNode.next;
-        newNode.prev=oldNode; 
-        oldNode.next.prev=newNode;
-        oldNode.next=newNode;
+    private void insertAfter(Node oldNode, Node newNode) {
+        newNode.next = oldNode.next;
+        newNode.prev = oldNode;
+        oldNode.next.prev = newNode;
+        oldNode.next = newNode;
     }
 
-    private void removeNode(Node node){
-        node.prev.next=node.next;
-        node.next.prev=node.prev;
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
         size--;
     }
 
-    private Node findNode(Object o){
-        Node node=head.next;
-        while (node!=tail && !node.val.equals(o)) node=node.next;
-        if (node!=tail) return node;
+    private Node findNode(Object o) {
+        Node node = head.next;
+        while (node != tail && !node.val.equals(o)) node = node.next;
+        if (node != tail) return node;
         return null;
     }
 
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof LinkedDeque)) return false;
-        Iterator tit=iterator();
-        Iterator nit=((LinkedDeque) obj).iterator();
+        Iterator tit = iterator();
+        Iterator nit = ((LinkedDeque) obj).iterator();
 
 
-        while (tit.hasNext()){
+        while (tit.hasNext()) {
             if (!nit.hasNext()) return false;
             if (!tit.next().equals(nit.next())) return false;
         }
@@ -242,10 +252,10 @@ public class LinkedDeque<E> implements Collection<E> {
 
     @Override
     public String toString() {
-        Iterator<E> it=iterator();
-        StringBuilder sb=new StringBuilder();
+        Iterator<E> it = iterator();
+        StringBuilder sb = new StringBuilder();
         sb.append("[");
-        while (it.hasNext()){
+        while (it.hasNext()) {
             sb.append(it.next().toString()).append(" ");
         }
         sb.append("]");
