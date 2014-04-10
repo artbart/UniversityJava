579b6634b96a0f2812eea018c08adae677af8760
artbart <ar.bartashev@gmail.com>
2014-04-11 00:11:35 +0400

    add main class

---
+++ src/main/java/IARunner.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+/**
+ * Created by arhont on 4/11/2014.
+ */
+public class IARunner {
+    public static void main(String[] args) {
+        if (args.length!=2){
+            System.out.println("<input file path> <output file path>");
+        }
+        try {
+            new InstructionsHandler(args[0],args[1]).handle();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
